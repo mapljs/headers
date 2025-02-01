2d15c356b90bdd2d206c09565fee1fe8ef779219
aquapi <aquaplmc@gmail.com>
2025-02-01 16:08:20 +0700

    CORS

--- src/cors.ts
+++ src/cors.ts
@@ -1,30 +1,52 @@
-export type HeaderValues = string[] | ('*' | (string & {}));
+import type { RouterPlugin, Router } from '@mapl/app';
+import { setMinimumHolders } from '@mapl/app/compiler/middleware.js';
+import type { MacroFunc } from '@mapl/app/macro.js';
+
+type HeaderValue = '*' | (string & {}) | [string, string, ...string[]];
 
 export interface Options {
-  allowHeaders?: HeaderValues;
-  allowMethods?: HeaderValues;
-  exposeHeaders?: HeaderValues;
+  allowHeaders?: HeaderValue;
+  allowMethods?: HeaderValue;
+  exposeHeaders?: HeaderValue;
   maxAge?: number;
   allowCredentials?: boolean;
 }
 
-/**
- * Headers that allow all origins
- */
-export const allowAllOrigins: [string, string][] = [['Access-Control-Allow-Origin', '*']];
-export const varyOrigin: [string, string] = ['Vary', 'Origin'];
+const corsMacro: MacroFunc<Exclude<HeaderValue, string>> = (options, ctx, state) => {
+  setMinimumHolders(ctx, 1);
 
-/**
- * Allow a single origin
- */
-export const allowOrigin = (origin: string): [string, string][] => [['Access-Control-Allow-Origin', origin], varyOrigin];
+  // eslint-disable-next-line
+  const originList = 'f' + state.externalValues.push(options);
+  ctx[0] += `${compilerConstants.HOLDER_0}=${compilerConstants.ORIGIN_VAL};${compilerConstants.HEADERS}.push(['Access-Control-Allow-Origin',${originList}.includes(${compilerConstants.HOLDER_0})?${compilerConstants.HOLDER_0}:${originList}[0]]);`;
+};
 
-/**
- * CORS headers parser
- */
-export const headers = (o: Options): [string, string][] => Object.entries(o).map(([x, y]) => [
-  // eslint-disable-next-line
-  'Access-Control-' + x[0].toUpperCase() + x.slice(1).replace(/[a-z][A-Z]/g, '$1-$2'),
-  // eslint-disable-next-line
-  '' + y
-]);
+export default ((app, origins, options) => {
+  // Optimized for code size
+  const headers: [string, string][] = options == null
+    ? []
+    : Object.entries(options).map(([x, y]) => [
+      // eslint-disable-next-line
+      'Access-Control-' + x[0].toUpperCase() + x.slice(1).replace(/([a-z])([A-Z])/g, '$1-$2'),
+      // eslint-disable-next-line
+      '' + y
+    ]);
+
+  // When one specific or multiple origins are specified
+  if (origins !== '*') {
+    headers.push(['Vary', 'Origin']);
+
+    // Handle multiple origins
+    if (Array.isArray(origins)) {
+      return app
+        .headers(headers)
+        .macro({
+          loadSource: corsMacro,
+          options: origins
+        });
+    }
+  }
+
+  // Decide right away
+  headers.push(['Access-Control-Allow-Origin', origins]);
+  return app.headers(headers);
+}) as RouterPlugin<Router, [origins: HeaderValue, options?: Options]>;
